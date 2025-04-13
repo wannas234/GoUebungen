958dce255e54b53181fbc6c77c22d7e0e32933b2
alfnas3 <s242222@student.dhbw-mannheim.de>
2025-04-13 11:21:38 +0200

    Akt

---
+++ aufgabe3.2/aufgabe3.2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Aufgabe3.2: Führe Addition und Subtraktion mit Variablen a und b durch
+func main() {
+	a := 10
+	b := 4
+
+	addition := a + b
+	subtraktion := a - b
+
+	fmt.Println("Addition:", addition)
+	fmt.Println("Subtraktion:", subtraktion)
+}
